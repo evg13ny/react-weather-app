406ebc430584c78d7342188b30d2e2b7953b70ef
Evgeny Polyakov <evg_pol@yahoo.com>
2022-11-03 22:13:12 +0300

    first commit

---
+++ src/api.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '5621750e05msh7bd3f106b43c646p11e8ecjsn7b885dd0947c',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
